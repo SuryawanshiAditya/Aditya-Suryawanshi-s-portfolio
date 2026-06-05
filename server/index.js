df04c305295e7e73187ac577db5a04d093d8d2e8
SuryawanshiAditya <suryawanshiadityaj@gmail.com>
2026-06-05 18:26:42 +0530

    cors allow correct vercel url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://aditya-suryawanshis-portfolio.vercel.app',
+    'https://adityasuryawanshisportfolio.vercel.app',
     'https://aditya-suryawanshi-s-portfolio.vercel.app',
     process.env.FRONTEND_URL
   ],
